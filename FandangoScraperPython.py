fca320171e53eb38cccaad42b0f02fe79c6f0be0
lidarwin <darwinli.sf@gmail.com>
2018-03-16 12:03:07 -0700

    Seems to be working consistently with unlocking the /napi/ of Fandango

--- FandangoScraperPython.py
+++ FandangoScraperPython.py
@@ -2,6 +2,7 @@ import os
 import requests
 from bs4 import BeautifulSoup
 import datetime
+import time
 import re
 
 
@@ -47,8 +48,13 @@ sTomorrow = str(datetime.date.today() + datetime.timedelta(1))
 
 #Since we need a header to make it look like we are visiting on a desktop
 userAgentHeader = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+userAgentHeader={'User-agent': 'Mozilla/5.0'}
 
 itest=0
+
+rMovieTimes=False
+rUnlocker=False
+
 #Now go through every Theater in America
 for theaterLink in theaterLinks:
     itest = itest+1
@@ -56,22 +62,37 @@ for theaterLink in theaterLinks:
         break;
     #Strip is necessary because there is a /n at the end
     theaterLink=theaterLink.strip()
-    #Need to find the code in the theater url. In the textfile, it follows underscore, but underscores become hyphens when going to the url
+    #Need to find the code in the theater url. In TheaterLinks.txt, it follows underscore, but underscores become hyphens after the URL redirects
     m = re.search('.*_(.*)\/theaterpage', theaterLink)
     theaterCode = m.group(1)
     theaterLink = requests.get(theaterLink).url
+    theaterSoup = soupLink(theaterLink)
     theaterLinkReqMovieTime='https://www.fandango.com/napi/theaterMovieShowtimes/'+theaterCode+'?startDate='+sTomorrow+'&isdesktop=true'
     theaterLinkReqMovieTimeValue=theaterLink+'?date='+sTomorrow
     
     data={'referer':theaterLinkReqMovieTimeValue}
-    headers = {**data, **headers}
+    headers = {**data, **userAgentHeader}
     
-    #For some reason, we need to REQUEST.GET this url with key/value so that our IP will be unlocked or something
+    #For some reason, we need to REQUEST.GET this url below with key/value so that our IP will be unlocked or something
     #r = requests.get('https://www.fandango.com/napi/nearbyTheaters?limit=7&zipCode=99515', data = {'referer':'https://www.fandango.com/regal-dimond-center-9-cinemas-aacwx/theater-page?date=2018-03-16'},headers=headers)
-    rMovieTimes = requests.get(theaterLinkReqMovieTime, data = data)
-    r = requests.get('https://www.fandango.com/napi/nearbyTheaters?limit=7&zipCode=99515', headers = {'User-Agent': 'Chrome/39.0.2171.95', 'referer':'https://www.fandango.com/regal-dimond-center-9-cinemas-aacwx/theater-page?date=2018-03-16'})
-    
-    r = requests.get('https://www.fandango.com/napi/theaterMovieShowtimes/aacwx?startDate=2018-03-16&isdesktop=true', headers = {'referer':'https://www.fandango.com/regal-dimond-center-9-cinemas-aacwx/theater-page?date=2018-03-16','User-Agent': 'Chrome/39.0.2171.95'})
-    
-    
+    #So, we basically need the zipcode of the theater
+    zipElement=theaterSoup.find_all("div", {"class": "js-closestTheaters-lazy"})
+    sZipCode=zipElement[0].get('data-theater-zip')
+    theaterUnlockerLink = 'https://www.fandango.com/napi/nearbyTheaters?limit=7&zipCode=' + sZipCode
+    rUnlocker = requests.get(theaterUnlockerLink, headers=headers)
+    while(True):
+        if ('Not Authorized' in rUnlocker.text):
+            print('Not Authorized when requesting the zipcode link')
+            time.sleep(0.5)
+            rUnlocker = requests.get(theaterUnlockerLink, headers=headers)
+        else:
+            break
+    rMovieTimes = requests.get(theaterLinkReqMovieTime, headers=headers)
+    while(True):
+        if ('Not Authorized' in rUnlocker.text):
+            print('Not Authorized when getting the Movie times')
+            time.sleep(0.5)
+            rMovieTimes = requests.get(theaterLinkReqMovieTime, headers=headers)
+        else:
+            break
     
