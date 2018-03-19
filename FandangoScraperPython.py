b9f2783479849baa424b77785443dfe37367a552
lidarwin <darwinli.sf@gmail.com>
2018-03-18 23:00:18 -0700

    Added calculation portion and trying to add a main function

--- FandangoScraperPython.py
+++ FandangoScraperPython.py
@@ -12,23 +12,12 @@ import time
 import re
 
 from selenium import webdriver
-import selenium.webdriver.chrome.service as service
+import selenium.webdriver.chrome.service as serv
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.select import Select
 
 #https://medium.com/@pyzzled/running-headless-chrome-with-selenium-in-python-3f42d1f5ff1d
 
-
-#List of all urls to every theater in America that is on Fandango.com
-theaterLinks=[]
-
-#List of all urls to every movie at every movietime sold on Fandango.com
-#This list will get output in text file on the fly in case something goes wrong
-movieTicketLinks=[]
-
-#Dictionary of every ticket price in USA that is purchasable on Fandango.
-#Maps float dollar ammount to a list of theater links. The information of the movie itself and the type (Adult, Child, Senior) is lost
-ticketPrices = {}
     
 def soupLink(url, baseURL='', headers={}):
     """ Takes in a URL and proceeds to return a BeautifulSoup HTML parsed object. BASEURL is required when the URL does not have HTTPS://
@@ -37,42 +26,9 @@ def soupLink(url, baseURL='', headers={}):
     c = r.content
     return BeautifulSoup(c,'html.parser')
 
-#We don't want to have to run the script to generate the theater links every time, so we check if the file exists
-if (not os.path.isfile('TheaterLinks.txt')):
-    #The starting URL. It has Fandango's list of states which are links to the theaters in the states
-    startURL = 'https://www.fandango.com/site-index/movietheaters.html'
-    
-    soup = soupLink(startURL)
-    
-    #List of all links to the state webpages
-    #We only want the states in USA, not Canada, and other islands
-    stateLinks = soup.find_all("a")
-    stateLinks = stateLinks[0:52]
-    
-    with open('TheaterLinks.txt', 'a') as the_file:
-        #To traverse each of the states, we need this base url
-        baseURL = 'https://www.fandango.com/site-index/'
-        for astate in stateLinks:
-            soupstate = soupLink(astate['href'], baseURL)
-            stateTheaterLinks = soupstate.find_all("a")
-            for aStateTheater in stateTheaterLinks:
-                theaterLinks.append(aStateTheater['href'])
-                the_file.write(aStateTheater['href'] + '\n')                
-else:
-    with open('TheaterLinks.txt', "r") as myfile:
-        theaterLinks = myfile.readlines()
-        
-        
-# instantiate a chrome options object so you can set the size and headless preference
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--window-size=1920x1080")
-
-# current directory
-chrome_driver = os.getcwd() +"\\chromedriver.exe"
 
 def getPrices(ticketingUrl, theaterLink, ticketPrices):
-    """ Takes in a TICKETINGURL and proceeds to checkout with all possible ticket types (Adult, Child, Senior). Saves the price for each ticket (with fandang convenience fee) as keys in a dictionary TICKETPRICES with the values as the THEATERLINK
+    """ Takes in a TICKETINGURL and proceeds to checkout with all possible ticket types (Adult, Child, Senior). Saves the price for each ticket (with fandang convenience fee) as keys in a dictionary TICKETPRICES with the values as the TICKETINGURL
     """
     driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
     
@@ -95,7 +51,7 @@ def getPrices(ticketingUrl, theaterLink, ticketPrices):
         fTotal = float(usdTotal[1:])
         if fTotal in ticketPrices:
             break;
-        ticketPrices[fTotal] = theaterLink
+        ticketPrices[fTotal] = ticketingUrl
         print(str(fTotal))
         driver.quit()
         driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
@@ -117,91 +73,154 @@ def getPrices(ticketingUrl, theaterLink, ticketPrices):
         fTotal = float(usdTotal[1:])
         if fTotal in ticketPrices:
             break;
-        ticketPrices[fTotal] = theaterLink
+        ticketPrices[fTotal] = ticketingUrl
         print(str(fTotal))
         driver.quit()
         driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
         driver.get(ticketingUrl)
-
-
-
-#For formatting when doing the requests
-sTomorrow = str(datetime.date.today() + datetime.timedelta(1))
-
-#Since we need a header to make it look like we are visiting on a desktop
-userAgentHeader = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-userAgentHeader={'User-agent': 'Mozilla/5.0'}
-
-itest=0
-
-rMovieTimes=False
-rUnlocker=False
-
-#Start the Selenium server with the headless chrome driver
-service = service.Service(chrome_driver)
-service.start()
-#Start Selenium webdriver service on the server in headless mode
-driver = webdriver.Remote(service.service_url,   desired_capabilities=chrome_options.to_capabilities())
-
-with open('TicketTransactionLinks.txt', 'a') as the_file:
-    #Now go through every Theater in America
-    for theaterLink in theaterLinks:
-#        if (itest==0):
-#            itest = itest+1
-#            continue
-#        itest = itest+1
-#        if itest == 3:
-#            break;
-        #Strip is necessary because there is a /n at the end
-        theaterLink=theaterLink.strip()
-        #Need to find the code in the theater url. In TheaterLinks.txt, it follows underscore, but underscores become hyphens after the URL redirects
-        m = re.search('.*_(.*)\/theaterpage', theaterLink)
-        theaterCode = m.group(1)
-        theaterLink = requests.get(theaterLink).url
-        #Filters out the CLOSED theaters
-        if ('404' in theaterLink):
-            continue
-        theaterSoup = soupLink(theaterLink)
-        theaterLinkReqMovieTime='https://www.fandango.com/napi/theaterMovieShowtimes/'+theaterCode+'?startDate='+sTomorrow+'&isdesktop=true'
-        theaterLinkReqMovieTimeValue=theaterLink+'?date='+sTomorrow
         
-        data={'referer':theaterLinkReqMovieTimeValue}
-        headers = {**data, **userAgentHeader}
+
+def fandangoCalculate(target, prices):
+    dp=[0]*(target + 1)
+    #Store the index of dp as the key and the value as an array of prices that are needed with duplicates
+    dic = {}
+    prices = prices.sort()
+    for i in range(0,target):
+        for j in range(0, len(prices)):
+            if (prices[j] <= i):
+                dp[i] = max(dp[i], dp[i - prices[j]] + prices[j])
+                if (i in dic):
+                    dic[i] = dic[i].append(prices[j])
+    return dic[target]
+    
+
+def main():
+    #List of all urls to every theater in America that is on Fandango.com
+    theaterLinks=[]
+    
+    #List of all urls to every movie at every movietime sold on Fandango.com
+    #This list will get output in text file on the fly in case something goes wrong
+    movieTicketLinks=[]
+    
+    #Dictionary of every ticket price in USA that is purchasable on Fandango.
+    #Maps float dollar ammount to a list of theater links. The information of the movie itself and the type (Adult, Child, Senior) is lost
+    ticketPrices = {}
+    
+    #We don't want to have to run the script to generate the theater links every time, so we check if the file exists
+    if (not os.path.isfile('TheaterLinks.txt')):
+        #The starting URL. It has Fandango's list of states which are links to the theaters in the states
+        startURL = 'https://www.fandango.com/site-index/movietheaters.html'
         
-        #For some reason, we need to REQUEST.GET this url below with key/value so that our IP will be unlocked or something
-        #r = requests.get('https://www.fandango.com/napi/nearbyTheaters?limit=7&zipCode=99515', data = {'referer':'https://www.fandango.com/regal-dimond-center-9-cinemas-aacwx/theater-page?date=2018-03-16'},headers=headers)
-        #So, we basically need the zipcode of the theater
-        zipElement=theaterSoup.find_all("div", {"class": "js-closestTheaters-lazy"})
-        sZipCode=zipElement[0].get('data-theater-zip')
-        theaterUnlockerLink = 'https://www.fandango.com/napi/nearbyTheaters?limit=7&zipCode=' + sZipCode
-        rUnlocker = requests.get(theaterUnlockerLink, headers=headers)
-        for i in range(0,1000):
-            if ('Not Authorized' in rUnlocker.text):
-                print('Not Authorized when requesting the zipcode link')
-                time.sleep(0.5)
-                rUnlocker = requests.get(theaterUnlockerLink, headers=headers)
-            else:
-                break
-        rMovieTimes = requests.get(theaterLinkReqMovieTime, headers=headers)
-        for i in range(0,1000):
-            if ('Not Authorized' in rUnlocker.text):
-                print('Not Authorized when getting the Movie times')
-                time.sleep(0.5)
-                rMovieTimes = requests.get(theaterLinkReqMovieTime, headers=headers)
-            else:
-                break
-        try:
-            jMovieTimes = rMovieTimes.json()
-            #jMovieTimes['viewModel']['movies'][1]['variants'][0]['amenityGroups'][0]['showtimes'][1]['ticketingUrl']
-            jMovieTimes=jMovieTimes['viewModel']['movies']
-            for movie in jMovieTimes:
-                showtimes = movie['variants'][0]['amenityGroups'][0]['showtimes']
-                for showtime in showtimes:
-                    ticketingUrl=showtime['ticketingUrl']
-                    movieTicketLinks.append(ticketingUrl)
-                    the_file.write(ticketingUrl + '\n')
-                    getPrices(ticketingUrl, theaterLink, ticketPrices)
-        except KeyboardInterrupt:
-            1+1==2
-        except:
-            continue
+        soup = soupLink(startURL)
+        
+        #List of all links to the state webpages
+        #We only want the states in USA, not Canada, and other islands
+        stateLinks = soup.find_all("a")
+        stateLinks = stateLinks[0:52]
+        
+        with open('TheaterLinks.txt', 'a') as the_file:
+            #To traverse each of the states, we need this base url
+            baseURL = 'https://www.fandango.com/site-index/'
+            for astate in stateLinks:
+                soupstate = soupLink(astate['href'], baseURL)
+                stateTheaterLinks = soupstate.find_all("a")
+                for aStateTheater in stateTheaterLinks:
+                    theaterLinks.append(aStateTheater['href'])
+                    the_file.write(aStateTheater['href'] + '\n')                
+    else:
+        with open('TheaterLinks.txt', "r") as myfile:
+            theaterLinks = myfile.readlines()
+            
+            
+    # instantiate a chrome options object so you can set the size and headless preference
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--window-size=1920x1080")
+    
+    # current directory
+    chrome_driver = os.getcwd() +"\\chromedriver.exe"
+    
+    
+    #For formatting when doing the requests
+    sTomorrow = str(datetime.date.today() + datetime.timedelta(1))
+    
+    #Since we need a header to make it look like we are visiting on a desktop
+    userAgentHeader = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    userAgentHeader={'User-agent': 'Mozilla/5.0'}
+    
+    itest=0
+    
+    rMovieTimes=False
+    rUnlocker=False
+    
+    #Start the Selenium server with the headless chrome driver
+    service = serv.Service(chrome_driver)
+    service.start()
+    #Start Selenium webdriver service on the server in headless mode
+    driver = webdriver.Remote(service.service_url,   desired_capabilities=chrome_options.to_capabilities())
+    
+    with open('TicketTransactionLinks.txt', 'a') as the_file:
+        #Now go through every Theater in America
+        for theaterLink in theaterLinks:
+    #        if (itest==0):
+    #            itest = itest+1
+    #            continue
+    #        itest = itest+1
+    #        if itest == 3:
+    #            break;
+            #Strip is necessary because there is a /n at the end
+            theaterLink=theaterLink.strip()
+            #Need to find the code in the theater url. In TheaterLinks.txt, it follows underscore, but underscores become hyphens after the URL redirects
+            m = re.search('.*_(.*)\/theaterpage', theaterLink)
+            theaterCode = m.group(1)
+            theaterLink = requests.get(theaterLink).url
+            #Filters out the CLOSED theaters
+            if ('404' in theaterLink):
+                continue
+            theaterSoup = soupLink(theaterLink)
+            theaterLinkReqMovieTime='https://www.fandango.com/napi/theaterMovieShowtimes/'+theaterCode+'?startDate='+sTomorrow+'&isdesktop=true'
+            theaterLinkReqMovieTimeValue=theaterLink+'?date='+sTomorrow
+            
+            data={'referer':theaterLinkReqMovieTimeValue}
+            headers = {**data, **userAgentHeader}
+            
+            #For some reason, we need to REQUEST.GET this url below with key/value so that our IP will be unlocked or something
+            #r = requests.get('https://www.fandango.com/napi/nearbyTheaters?limit=7&zipCode=99515', data = {'referer':'https://www.fandango.com/regal-dimond-center-9-cinemas-aacwx/theater-page?date=2018-03-16'},headers=headers)
+            #So, we basically need the zipcode of the theater
+            zipElement=theaterSoup.find_all("div", {"class": "js-closestTheaters-lazy"})
+            sZipCode=zipElement[0].get('data-theater-zip')
+            theaterUnlockerLink = 'https://www.fandango.com/napi/nearbyTheaters?limit=7&zipCode=' + sZipCode
+            rUnlocker = requests.get(theaterUnlockerLink, headers=headers)
+            for i in range(0,1000):
+                if ('Not Authorized' in rUnlocker.text):
+                    print('Not Authorized when requesting the zipcode link')
+                    time.sleep(0.5)
+                    rUnlocker = requests.get(theaterUnlockerLink, headers=headers)
+                else:
+                    break
+            rMovieTimes = requests.get(theaterLinkReqMovieTime, headers=headers)
+            for i in range(0,1000):
+                if ('Not Authorized' in rUnlocker.text):
+                    print('Not Authorized when getting the Movie times')
+                    time.sleep(0.5)
+                    rMovieTimes = requests.get(theaterLinkReqMovieTime, headers=headers)
+                else:
+                    break
+            try:
+                jMovieTimes = rMovieTimes.json()
+                #jMovieTimes['viewModel']['movies'][1]['variants'][0]['amenityGroups'][0]['showtimes'][1]['ticketingUrl']
+                jMovieTimes=jMovieTimes['viewModel']['movies']
+                for movie in jMovieTimes:
+                    showtimes = movie['variants'][0]['amenityGroups'][0]['showtimes']
+                    for showtime in showtimes:
+                        ticketingUrl=showtime['ticketingUrl']
+                        movieTicketLinks.append(ticketingUrl)
+                        the_file.write(ticketingUrl + '\n')
+                        getPrices(ticketingUrl, theaterLink, ticketPrices)
+            except KeyboardInterrupt:
+                return
+            except:
+                continue
+            
+if __name__ == '__main__':
+    main()
